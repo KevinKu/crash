330c18239539cb03d36aa896cdbc065fbcf1db36
proof.K <koftwro@gmail.com>
2021-08-30 12:25:34 +0800

    2
    2

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { PushNotifications } from "@capacitor/push-notifications";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  async created() {
+    PushNotifications.createChannel({
+      id: "start",
+      description: "hihi world",
+      sound: "a",
+      importance: 4,
+    });
+
+    PushNotifications.requestPermissions().then(function (re) {
+
+      if (re.receive === "granted") {
+        PushNotifications.register();
+      }
+    });
+
+    PushNotifications.addListener("registration", async function (token) {
+
+      console.log(token["value"]);
+
+      
+    });
+
+    /* 收到推播通知時的處理 */
+    PushNotifications.addListener("pushNotificationReceived", (notice) => {
+
+      console.log(JSON.stringify(notice));
+
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
